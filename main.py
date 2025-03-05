f17b9cbcc8be25a0eeb8947e36746897a372dacb
StAl15 <stihinaleksander@yandex.ru>
2025-03-06 01:10:35 +0500

    add otp

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import RedirectResponse, Response
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
-import logging
 import base64
+import logging
+import random
+import string
+import time
+from threading import Thread
+
+from fastapi import FastAPI, Form, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 allowed_values = {
-    "Катя", 
-    "Маша", 
-    "Алёна", 
-    "Милана", 
-    "Полина Б.", 
-    "Полина С.", 
+    "Катя",
+    "Маша",
+    "Алёна",
+    "Милана",
+    "Полина Б.",
+    "Полина С.",
     "Настя"
 }
 second_task_code = "1234567890"
@@ -34,9 +39,11 @@ app.add_middleware(
 
 logging.basicConfig(level=logging.INFO)
 
+
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/")
 async def submit_form(value: str = Form(...)):
@@ -49,11 +56,12 @@ async def submit_form(value: str = Form(...)):
     else:
         return RedirectResponse(url="/", status_code=303)
 
+
 @app.get("/guesswho")
 async def guess_who(request: Request):
     code = request.query_params.get("code")
     name = request.query_params.get("name")
-    
+
     logging.info(f"Code from query: {code}, Name from query: {name}")
 
     encoded_value = request.cookies.get("user_value")
@@ -69,13 +77,59 @@ async def guess_who(request: Request):
 
     return templates.TemplateResponse("guesswho.html", {"request": request, "user_value": user_value})
 
+
 @app.get("/authors")
 async def authors(request: Request):
     return templates.TemplateResponse("authors.html", {"request": request})
 
+
 @app.get("/youneedtobefaster")
 async def youneedtobefaster(request: Request):
     return templates.TemplateResponse("youneedtobefaster.html", {"request": request})
+
+
+temp_password = ""
+time_left = 10  # Время до обновления пароля в секундах
+
+
+def generate_temp_password():
+    global temp_password, time_left
+    while True:
+        temp_password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        time_left = 10  # После обновления пароля обнуляем счетчик
+
+        # Уменьшаем счетчик времени до обновления
+        for _ in range(10):
+            time.sleep(1)
+            time_left -= 1
+
+
+@app.on_event("startup")
+def start_password_generation():
+    # Запускаем генерацию пароля в отдельном потоке
+    password_thread = Thread(target=generate_temp_password)
+    password_thread.daemon = True
+    password_thread.start()
+
+
+@app.get("/tmp_key")
+def get_temp_password():
+    return {
+        "tmp_key": temp_password,
+        "time_left": time_left
+    }
+
+
+@app.get("/sendme")
+def get_temp_password(key: str):
+    if key == temp_password:
+        return {
+            'new_link': '/LOWER_CASE'
+        }
+    else:
+        return {
+            "error": 'Попытка не пытка'
+        }
 
 # @app.post("/lowercase")
 # async def lowercase(request: Request):
